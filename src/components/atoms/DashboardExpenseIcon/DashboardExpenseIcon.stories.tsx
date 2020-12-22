41f2102bfa99d1f0418902536ea3f42758719034
barsh11 <barsh11@gmail.com>
2020-12-22 10:47:30 +0200

    added expense and income icon for dashboard

---
+++ src/components/atoms/DashboardExpenseIcon/DashboardExpenseIcon.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DashboardExpenseIcon from './DashboardExpenseIcon';
+
+export default {
+  title: 'Sidebar/DashboardExpenseIcon',
+};
+
+export const common = () => <DashboardExpenseIcon>common DashboardExpenseIcon</DashboardExpenseIcon>;
---
+++ src/components/atoms/DashboardIncomeIcon/DashboardIncomeIcon.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DashboardIncomeIcon from './DashboardIncomeIcon';
+
+export default {
+  title: 'Sidebar/DashboardIncomeIcon',
+};
+
+export const common = () => <DashboardIncomeIcon>common DashboardIncomeIcon</DashboardIncomeIcon>;
