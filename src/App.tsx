e93a3798f8680d84f02beb29c00ff61371ea4ebf
barsh11 <barsh11@gmail.com>
2021-01-04 20:08:34 +0200

    reached to a stage where transaction card is rendered while clicked in summary, adjusments are needed

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import UserContextProvider from './contexts/UserContext';
 import DataContextProvider from './contexts/DataContext';
+import AppContextProvider from './contexts/AppContext';
 import Homepage from './views/pages/Homepage';
 
 const App: React.FC = () => (
   <BrowserRouter>
     <UserContextProvider>
       <DataContextProvider>
-        <Homepage />
+        <AppContextProvider>
+          <Homepage />
+        </AppContextProvider>
       </DataContextProvider>
     </UserContextProvider>
   </BrowserRouter>
---
+++ src/views/pages/SingleTransaction/SingleTransaction.tsx
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { DataContext, DataItemProps } from '../../../contexts/DataContext';
+import TransactionCard from '../../../components/molecules/TransactionsCard/TransactionsCard';
+import narrowTransactionType from '../../../utils/narrowTransactionType';
+
+type singleTransaction = {
+  chosenId: string;
+};
+
+const SWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100%;
+  width: 100%;
+`;
+
+const SingleTransaction: React.FC<singleTransaction> = ({ chosenId }) => {
+  const transactionsList = useContext(DataContext);
+  const transactionItem: DataItemProps = transactionsList.filter((curr) => curr.id === chosenId)[0];
+
+  return (
+    <SWrapper>
+      {transactionItem ? (
+        <TransactionCard
+          type={narrowTransactionType(transactionItem.transactionType)}
+          amount={parseFloat(transactionItem.amount)}
+          currency={transactionItem.currency}
+          isRefund={transactionItem.transactionType === 'cancelled'}
+          company={transactionItem.company}
+          time={transactionItem.time}
+          date={transactionItem.date}
+          id={transactionItem.id}
+          isStarred={transactionItem.isStarred}
+        />
+      ) : null}
+    </SWrapper>
+  );
+};
+
+export default SingleTransaction;
