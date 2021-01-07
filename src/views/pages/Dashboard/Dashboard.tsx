6f4d7ff44510e92321c081018f5d76f7654132b0
barsh11 <barsh11@gmail.com>
2021-01-07 15:07:53 +0200

    implemented transactions dashboard UI

--- src/views/pages/Dashboard/Dashboard.tsx
+++ src/views/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CreditCard from '../../../components/molecules/CreditCard/CreditCard';
+import { UserContext } from '../../../contexts/UserContext';
 import SWrapper from '../MainStyle';
+import DashboardTransactions from '../../../components/molecules/DashboardTransactions/DashboardTransactions';
 
-const Dashboard: React.FC = () => (
-  <SWrapper>
-    <CreditCard last4={4115} cardDate="04/21" cardName="Ian Kelley" />
-  </SWrapper>
-);
+const Dashboard: React.FC = () => {
+  const user = useContext(UserContext);
+
+  return (
+    <SWrapper>
+      <CreditCard
+        last4={user.creditCard.last4Digits}
+        cardDate={`${user.creditCard.expMonth}/${user.creditCard.expYear}`}
+        cardName={`${user.firstName} ${user.lastName}`}
+      />
+      <div />
+      <DashboardTransactions type="expense" currency="USD" revenue={90524} />
+      <DashboardTransactions type="Income" currency="USD" revenue={60228} />
+    </SWrapper>
+  );
+};
 
 export default Dashboard;
