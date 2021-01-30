d9b9e536e58326d41eb8c47e0f04dc075af01171
barsh11 <barsh11@gmail.com>
2021-01-30 16:38:22 +0200

    handled currency which are not in list

--- src/utils/convertCurrency.ts
+++ src/utils/convertCurrency.ts
@@ -4,7 +4,10 @@ const convertCurrency = (
   toCurr: string,
   rates: { [key: string]: number }
 ): number => {
-  const ratio1: number = rates[fromCurr];
+  let ratio1: number = rates[fromCurr];
+  if (ratio1 === undefined) {
+    ratio1 = rates.EUR;
+  } // if currency not in list- assume EUR
   const ratio2: number = rates[toCurr];
   const total: number = (amount / ratio1) * ratio2;
   if (Number.isNaN(total)) {
